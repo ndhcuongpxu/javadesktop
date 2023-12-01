0143f7047d834e8a0b64fc9436f98dc596dbc04b
WINDOWS <WINDOWS@WINDOWS-PC>
2023-12-01 09:36:19 +0700

    Xuat nhap du lieu

---
+++ ontapjava/src/kiemthu/XuatNhapDuLieu.java
@@ -0,0 +1,16 @@
+package kiemthu;
+
+import java.util.Scanner;
+
+public class XuatNhapDuLieu {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world!!!");
+		System.out.print("Username: ");
+		Scanner sc = new Scanner(System.in);
+		String u = sc.nextLine();
+		System.out.println("Ban da nhap: " + u);
+	}
+
+}
